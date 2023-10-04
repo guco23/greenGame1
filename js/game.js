0ee13e65e84928b77f03bc08313980eafb507fc6
Diego Montenegro Calvo <73708155+guco23@users.noreply.github.com>
2023-10-04 16:06:40 +0200

    mas cosillas

--- js/game.js
+++ js/game.js
@@ -2,3 +2,9 @@ var canvas = document.querySelector('canvas').getContext('2d');
 
 canvas.fillStyle = "green";
 canvas.fillRect(10,12,120,100);
+
+canvas.fillStyle = "red";
+canvas.fillRect(30,32,120,100);
+
+canvas.fillStyle = "blue";
+canvas.fillRect(54,17,120,100);
