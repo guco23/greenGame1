423b95e638fd4d31231acda5e2c199d2db554d8f
Diego Montenegro Calvo <73708155+guco23@users.noreply.github.com>
2023-10-10 12:48:27 +0200

    problema arreglado uwu

--- js/game.js
+++ js/game.js
@@ -1,23 +1,24 @@
-//Elemento visual provisional
-var canvas = document.querySelector('canvas').getContext('2d');
-
-canvas.fillStyle = "green";
-canvas.fillRect(10,12,120,100);
-
-canvas.fillStyle = "red";
-canvas.fillRect(30,32,120,100);
-
-canvas.fillStyle = "blue";
-canvas.fillRect(54,17,120,100);
 
 var config = {
     type: Phaser.AUTO,
-    width: 800,
+    width: 900, //Configurar aquí el tamaño de la ventana de juego
     height: 600,
     scene: {
         preload: preload,
-        create: create
-    }
+        create: create,
+        update: update
+    },
+    parent: 'espacio-juego'
 };
 
 var game = new Phaser.Game(config);
+
+//preload assets and data function
+function preload() {
+  
+}
+
+//create function
+function create() {
+
+}
