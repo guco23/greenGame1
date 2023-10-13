88a1c5eea9eb1103ac98e1b87fca20eb1c2bfaca
Diego Montenegro Calvo <73708155+guco23@users.noreply.github.com>
2023-10-13 18:24:33 +0200

    version 0.01: javier

--- js/game.js
+++ js/game.js
@@ -3,21 +3,24 @@ var config = {
     type: Phaser.AUTO,
     width: 900, //Configurar aquí el tamaño de la ventana de juego
     height: 600,
-    scene: {
+    pixelArt: true,
+    scene: [{
+        //init: init
         preload: preload,
         create: create,
-    },
+        //update: update
+    }],
     parent: 'espacio-juego'
 };
 
 var game = new Phaser.Game(config);
 
-//preload assets and data function
+//cargar aqui los datos de la escena.
 function preload() {
-      this.load.image('javier', './images/javier.jpg');
+      this.load.image('javier', '../images/javier.jpg');
 }
 
-//create function
+//crear aqui los objetos de la escena
 function create() {
-
+    this.add.image(400, 300, 'javier');
 }
