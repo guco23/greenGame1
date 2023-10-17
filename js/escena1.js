df7deda15eaa6fe88361fa39e51553dead6c9348
Diego Montenegro <dmonoficial22@gmail.com>
2023-10-17 13:42:54 -0700

    animacion chuli

--- js/escena1.js
+++ js/escena1.js
@@ -1,11 +1,27 @@
 export class Escena1 extends Phaser.Scene {
     //cargar aqui los datos de la escena.
+
     preload() {
         this.load.image('javier', '../images/javier.jpg');
+        this.upscaleval = 0.001;
+
     }
 
     //crear aqui los objetos de la escena
     create() {
-        this.add.image(400, 300, 'javier').setScale(0.7, 0.7); //omg so sexy
+        let screenWidth = this.game.config.width;
+        let screenHeight = this.game.config.height;
+        //Imagen 1
+        this.image = this.add.image(screenWidth, screenHeight, 'javier'); //omg so sexy
+        this.image.setScale(0.3);
+        this.image.setPosition(screenWidth / 2, screenHeight / 2);
+    }
+
+    update() {
+        this.image.scale += this.upscaleval;
+        if (this.image.scale > 0.6)
+            this.upscaleval = -0.002;
+        else if (this.image.scale < 0.2)
+            this.upscaleval = 0.003;
     }
 };
