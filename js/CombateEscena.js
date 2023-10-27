e92c8ac4faee27d9de4513ea8ab44ba6e3a8afb1
diego <micorreolojuro@si>
2023-10-27 17:59:58 +0200

    nueva licencia y nuevo combate escena

---
+++ js/CombateEscena.js
@@ -0,0 +1,15 @@
+export class Escena1 extends Phaser.Scene {
+    //cargar aqui los datos de la escena.
+    preload() {
+
+    }
+
+    //crear aqui los objetos de la escena
+    create() {
+
+    }
+
+    update() {
+
+    }
+};
