2c0ce4da6327715dea321f2d6463f989b395cf74
Diego Montenegro <dmonoficial22@gmail.com>
2023-10-30 08:44:03 -0700

    los sprites de los enemigos y aliados aparecen en la escena

--- js/CombateEscena.js
+++ js/CombateEscena.js
@@ -7,28 +7,43 @@ export class CombateEscena extends Phaser.Scene {
         //Primero, tomará un array con los enemigos y los aliados a partir de los cuales creará los objetos en pantalla
 
         //Placeholders
+        //Es importante que los sprites finales tengan la misma resolución
         let aliados = ['./images/javier.jpg', './images/javier.jpg', './images/javier.jpg', './images/javier.jpg']; //En la version final sacará esto de combatManager
         let enemigos = ['../images/furro.jpg', '../images/furro.jpg', '../images/furro.jpg', '../images/furro.jpg']; //En la version final sacará esto de combatManager
 
         //Añade las imagenes a la escena como enemigo/aliado y el numero que ocupan en su array
-        for(let i = 0; i < aliados.length; i++) {
+        for (let i = 0; i < aliados.length; i++) {
             this.load.image('aliado' + i, aliados[i]);
         }
-        for(let i = 0; i < enemigos.length; i++) {
+        for (let i = 0; i < enemigos.length; i++) {
             this.load.image('enemigo' + i, enemigos[i]);
         }
     }
 
     //crear aqui los objetos de la escena
     create() {
-        let aliados = ['aliado0', 'aliado1', 'aliado2', 'aliado3']; //En la version final sacará esto de combatManager
+        let aliados = ['aliado0', 'aliado1', 'aliado2']; //En la version final sacará esto de combatManager
         let enemigos = ['enemigo0', 'enemigo1', 'enemigo2', 'enemigo3']; //En la version final sacará esto de combatManager
 
-        for(let i = 0; i < aliados.length; i++) {
+        //Los datos tratados serán provenientes del gameManager, una vez esa parte esté hecha habrá que cambiar de donde se saca cada dato
+        for (let i = 0; i < aliados.length; i++) {
             this.load.image('aliado' + i, aliados[i]);
         }
-        for(let i = 0; i < enemigos.length; i++) {
+        for (let i = 0; i < enemigos.length; i++) {
             this.load.image('enemigo' + i, enemigos[i]);
+        } 
+
+        let gameWidth = this.game.config.width;
+        let gameHeight = this.game.config.height;
+
+        //Coloca los sprites de los enemigos en la escena
+        for (let i = 0; i < aliados.length; i++) {
+            let aliado = this.add.image(gameWidth / 5, gameHeight /  (aliados.length + 1) * (i + 1) , 'aliado' + i)
+            aliado.setScale(0.05);
+        }
+        for (let i = 0; i < enemigos.length; i++) {
+            let aliado = this.add.image(gameWidth - (gameWidth / 5), gameHeight /  (enemigos.length + 1) * (i + 1) , 'enemigo' + i)
+            aliado.setScale(0.05);
         }
     }
 
