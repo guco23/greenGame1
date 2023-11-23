678478351d32a87757f52428681d3f15c419c14e
Diego Montenegro <dmonoficial22@gmail.com>
2023-11-23 10:22:47 +0100

    Selector de accion funcional

--- src/js/CombateEscena.js
+++ src/js/CombateEscena.js
@@ -1,8 +1,10 @@
 import { RAIZ_IMAGENES } from "./constants.js";
-import {Enemigo} from "./Combate JS/Enemigos/Enemigo.js"
+import { Enemigo } from "./Combate JS/Enemigos/Enemigo.js"
 import { Personaje } from "./Combate JS/Personajes/Personaje.js"
 import { BarraVida } from "./HUDElems/BarraVida.js";
 import { TextoVida } from "./HUDElems/TextoVida.js";
+import { TextoDescriptivo } from "./HUDElems/TextoDescriptivo.js";
+import { SelectorAcciones } from "./HUDElems/SelectorAcciones.js";
 
 export class CombateEscena extends Phaser.Scene {
     //CombatManager combatManager;
@@ -27,13 +29,28 @@ export class CombateEscena extends Phaser.Scene {
         }
         //Carga el fondo, dependerá de la zona del juego en la que nos encontremos
         this.load.image('background', RAIZ_IMAGENES + "combatBackground/combatBackgroundPlaceholder.png");
+
+        //Controles en combate
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.input.keyboard.on('keydown', function (event) {
+            //Control
+            if (event.code === "ArrowUp") {
+                this.selectorAcciones.anterior();
+            } else if (event.code === "ArrowDown") {
+                this.selectorAcciones.siguiente();
+            }
+            else if (event.code === "Space" || event.code === "ArrowLeft") {
+                //SELECCIONAR
+                console.log("seleccionado" + this.selectorAcciones.selection);
+            }
+        }, this);
     }
 
     //crear aqui los objetos de la escena
     create() {
-                this.graphics = this.add.graphics();
+        this.graphics = this.add.graphics();
         //Arrays declarados provisionales para guardar los objetos de la escena
-         //En la version final aliados y enemigos serán de combatManager
+        //En la version final aliados y enemigos serán de combatManager
         this.aliados = [];
         this.aliados.push(new Personaje('Diego', 1, 30, 20, 120, 110));
         this.aliados.push(new Personaje('Pablo', 0, 30, 20, 140, 100));
@@ -51,7 +68,7 @@ export class CombateEscena extends Phaser.Scene {
         let uiBoxHeight = gameHeight / 3.2;
         let uiBoxWidth = gameWidth / 3;
         this.graphics.lineStyle(1, 0xffffff);
-        this.graphics.fillStyle(0x031f4c, 1);        
+        this.graphics.fillStyle(0x031f4c, 1);
         this.graphics.strokeRect(0, gameHeight - uiBoxHeight, uiBoxWidth, uiBoxHeight);
         this.graphics.fillRect(2, 150, 90, 100);
         this.graphics.strokeRect(95, 150, 90, 100);
@@ -59,15 +76,15 @@ export class CombateEscena extends Phaser.Scene {
         this.graphics.strokeRect(188, 150, 130, 100);
         this.graphics.fillRect(188, 150, 130, 100);
         //Coloca el fondo
-        this.add.image(gameWidth / 2, gameHeight / 2,'background')
+        this.add.image(gameWidth / 2, gameHeight / 2, 'background')
         //Coloca los sprites de los enemigos en la escena, en la versión final los personajes contienen sprites y su funcionalidad
         for (let i = 0; i < this.aliados.length; i++) {
-            this.add.image(gameWidth / 5, (gameHeight - gameHeight / 6)/  (this.aliados.length + 1) * (i + 1) - 50, 'aliado' + this.aliados[i].id).setScale(0.05);
+            this.add.image(gameWidth / 5, (gameHeight - gameHeight / 6) / (this.aliados.length + 1) * (i + 1) - 50, 'aliado' + this.aliados[i].id).setScale(0.05);
         }
-        
+
         this.imgsEnem = [];
         for (let i = 0; i < this.enemigos.length; i++) {
-            this.imgsEnem[i] = this.add.image(gameWidth - (gameWidth / 5),  (gameHeight - gameHeight / 6)/  (this.enemigos.length + 1) * (i + 1) - 40, 'enemigo' + this.enemigos[i].id).setScale(0.05);
+            this.imgsEnem[i] = this.add.image(gameWidth - (gameWidth / 5), (gameHeight - gameHeight / 6) / (this.enemigos.length + 1) * (i + 1) - 40, 'enemigo' + this.enemigos[i].id).setScale(0.05);
         }
 
         //Creación de los cuadros del HUD
@@ -85,9 +102,16 @@ export class CombateEscena extends Phaser.Scene {
 
         this.vidasEnemigos = [];
         for (let i = 0; i < this.enemigos.length; i++) {
-            this.vidasEnemigos.push(new BarraVida(this, this.imgsEnem[i].x - 40, this. imgsEnem[i].y - 43, 80, 18, this.enemigos[i].maxHp))
+            this.vidasEnemigos.push(new BarraVida(this, this.imgsEnem[i].x - 40, this.imgsEnem[i].y - 43, 80, 18, this.enemigos[i].maxHp))
         }
         //Prueba de la barra de vida
         this.vidasEnemigos[2].actualizarHp(25);
+
+        this.textoDescriptivo = new TextoDescriptivo(this, 420, 440);
+        this.selectorAcciones = new SelectorAcciones(this, 300, 440, this.textoDescriptivo);
+    }
+
+    OnKeyInput(event) {
+
     }
 };
