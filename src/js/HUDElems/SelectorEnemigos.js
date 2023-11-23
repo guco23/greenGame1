83632113db6f6111ae32cbb938b043c18244c6b8
Diego Montenegro <dmonoficial22@gmail.com>
2023-11-23 12:39:57 +0100

    menu de seleccion de enemigos

--- src/js/HUDElems/SelectorEnemigos.js
+++ src/js/HUDElems/SelectorEnemigos.js
@@ -1,37 +1,40 @@
-class AccionText extends Phaser.GameObjects.Container {
-    constructor(scene, x, y, text, textoDescriptivo) {
+class SelectorEnem extends Phaser.GameObjects.Container {
+    constructor(scene, x, y) {
         super(scene);
-        this.textoDescriptivo = textoDescriptivo;
-        this.textElem = scene.add.text(x, y, text);
+        //Construye un rectángulo hasta que tengamos un sprite de flecha
+        this.rect = scene.add.rectangle(x, y, 50, 50, 0xffffff);
+        this.unselect();
     }
 
     selected() {
-        this.textElem.setTint(0xFFFF00);
+        this.rect.visible = true;
     }
 
     unselect() {
-        this.textElem.setTint(0xFFFFFF);
+        this.rect.visible = false;
     }
 
 }
 
-export class SelectorAcciones extends Phaser.GameObjects.Container {
-
-    constructor(scene, x, y, textoDescriptivo) {
+export class SelectorEnemigos extends Phaser.GameObjects.Container {
+    //Necesita el array de enemigos para saber si están vivos y conocer su ubicación en la escena
+    constructor(scene, enemigos) {
         super(scene);
         this.selection = 0;
-        this.textoDescriptivo = textoDescriptivo;
+        //Crea las flechas
         this.opciones = [];
-        this.opciones.push(new AccionText(scene, x, y, "Atacar", "Inflige daño a un enemigo"));
-        this.opciones.push(new AccionText(scene, x, y + 40, "Habilidad", "TODO: depende de cada aliado"));
-        this.opciones.push(new AccionText(scene, x, y + 80, "Defender", "Reduce el daño recibido hasta el siguiente turno"));
+        for(let i = 0; i < enemigos.length; i++) {
+            //El metodo dependera de como sean los enemigos. Si finalmente heredan de sprite de phaser esto funcionará.
+            this.opciones.push(new SelectorEnem(scene, enemigos[i].x - 100, enemigos[i].y));
+        }
         this.select();
     }
 
+    /**
+     * Marca el enemigo como seleccionado. Metodo solo para usar desde la clase
+     */
     select() {
         this.opciones[this.selection].selected();
-        //Muestra la descripción de la acción
-        this.textoDescriptivo.aplicarTexto(this.opciones[this.selection].textoDescriptivo);
     }
 
     /**
@@ -39,6 +42,7 @@ export class SelectorAcciones extends Phaser.GameObjects.Container {
      */
     siguiente() {
         //Condición para evitar que se salga del array
+        
         if (this.selection < this.opciones.length - 1) {
             this.opciones[this.selection].unselect();
             this.selection = this.selection + 1;
@@ -59,16 +63,17 @@ export class SelectorAcciones extends Phaser.GameObjects.Container {
     }
 
     /**
-     * Oculta el menu de las acciones
+     * Oculta la UI de las acciones
      */
     ocultar() {
         this.visible = false;
     }
 
     /**
-     * Muestra el menu de las acciones
+     * Muestra la UI de las acciones
      */
     mostrar() {
         this.visible = true;
     }
+
 }
