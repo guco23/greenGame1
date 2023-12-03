86d931a0077877b3dc320dae9c0a93f05100e726
PablooMS <124464156+PablooMS@users.noreply.github.com>
2023-12-03 13:04:52 +0100

    Update AoE.js

--- src/js/Combate JS/Enemigos/AoE.js
+++ src/js/Combate JS/Enemigos/AoE.js
@@ -3,17 +3,34 @@ import { Enemigo } from "./Enemigos/Enemigo.js";
 export class AoE extends Enemigo {
 
     areaAttack() {
-        target = currentCombat.playerTeam;
+        let target = currentCombat.playerTeam;
         for(i = 0; i < currentCombat.teamSize; i++) {
             if(target.living) {
                 this.currentCombat.addInfo("attack", target[i].sufferDamage(this.atk / 2), this, target[i]);
                 target[i].checkAlive();
             }
         }
+        this.endTurn();
     }
 
     selectAction() {
-
+        let rand; //Hacer algo random entre 1 y 3
+        if(this.currentHP < this.maxHp / 2) {
+            if(rand === 0) {
+                this.attack();
+            }
+            else {
+                this.areaAttack();
+            }
+        }
+        else {
+            if(rand === 2) {
+                this.areaAttack();
+            }
+            else {
+                this.attack();
+            }
+        }
     }
 
     takeTurn() {
