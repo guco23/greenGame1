3969928ac1433068be2669fb6e657eab6278627f
Diego Montenegro <dmonoficial22@gmail.com>
2023-12-05 12:57:52 +0100

    estos cosillas

--- src/js/HUDElems/ScenePersonaje.js
+++ src/js/HUDElems/ScenePersonaje.js
@@ -1,12 +1,31 @@
+const statusIconSize = {
+    x: 20,
+    y: 20
+}
+
 class ScenePersonaje extends Phaser.GameObjects.Container {
     constructor(scene, x, y, img) {
         super(scene);
         this.img = scene.add.image(x, y, img).setScale(0.05);
         this.img.setOrigin(0, 0);
+        //Esto será un sprite cuando esté hecho
+        this.stun = scene.add.rectangle(x + this.img.displayWidth - statusIconSize.x, y + this.img.displayHeight - statusIconSize.y , statusIconSize.x, statusIconSize.y, 0xFFFF00).setOrigin(0,0);
+        this.poisoned = scene.add.rectangle(x + this.img.displayWidth - statusIconSize.x * 2, y + this.img.displayHeight - statusIconSize.y , statusIconSize.x, statusIconSize.y, 0x9402fc).setOrigin(0,0);
     }
 
     ocultar() {
         this.img.visible = false;
+        this.stun.visible = true;
+        this.poisoned.visible = true;
+
+    }
+
+    setPoisoned() {
+        this.poisoned.visible = true;
+    }
+
+    setStunned() {
+        this.stun.visible = true;
     }
 }
 
