e6f2438d5c84bcd5397c0f9cc0e3eba63b040328
PablooMS <124464156+PablooMS@users.noreply.github.com>
2023-12-17 11:41:47 +0100

    Activista y Mediador interchanged

---
+++ src/js/Combate JS/Personajes/Activista.js
@@ -0,0 +1,26 @@
+import { Personaje } from "./Personaje.js";
+
+export class Activista extends Personaje {
+    healMod;
+    buffMod;
+
+    constructor(idn) {
+        super(idn);
+        this.targetKind = 1;
+        this.healMod = 0.1;
+    }
+
+    special(target) {
+        let ally = this.currentCombat.playerTeam[target];
+        this.currentCombat.addInfo("special", this.name + " está aumentando la moral de su equipo", this, null);
+        for(i = 0; i < this.currentCombat.teamSize; i++) {
+            let ally = this.currentCombat.playerTeam[i];
+            if(ally.living) {
+                ally.heal(this.maxHp * this.healMod);
+            }
+        }
+        ally.modifyStat(true, this.buffMod, 1);
+        this.currentCombat.addInfo("special", ally.name + " fue especialmente inspirado por sus palabras.", this, null);
+        this.endTurn();
+    }
+}
--- src/js/Combate JS/Personajes/Mediador.js
+++ src/js/Combate JS/Personajes/Mediador.js
@@ -1,27 +1,26 @@
 import { Personaje } from "./Personaje.js";
 
 export class Mediador extends Personaje {
-
     healMod;
-    buffMod;
 
     constructor(idn) {
         super(idn);
         this.targetKind = 1;
-        this.healMod = 0.1;
+        this.healMod = 0.2;
     }
 
     special(target) {
         let ally = this.currentCombat.playerTeam[target];
-        this.currentCombat.addInfo("special", this.name + " está aumentando la moral de su equipo", this, null);
-        for(i = 0; i < this.currentCombat.teamSize; i++) {
-            let ally = this.currentCombat.playerTeam[i];
-            if(ally.living) {
-                ally.heal();
-            }
+        this.currentCombat.addInfo("special", this.name + " le está dando un tratamiento especial a " + ally.name, this, null);
+        ally.heal(this.maxHp * this.healMod);
+        if(ally.dot > 0) {
+            ally.dot = -5;
         }
-        ally.modifyStat(true, this.buffMod, 1);
-        this.currentCombat.addInfo("special", ally.name + " fue especialmente inspirado por sus palabras.", this, null);
+        else {
+            ally.applyDot(-3);
+        }
+        this.currentCombat.addInfo("special", ally.name + " se siente tan bien que ahora también cuida de sí mismo", this, null);
         this.endTurn();
     }
+
 }
