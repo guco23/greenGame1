a72044047aafd6e87eb31ba88b90c62183c634d5
JoseAntonioCA <jocarm02@ucm.es>
2023-12-20 12:18:51 +0100

    reajustados parametros de vida y ataque de items y enemigos

--- assets/EquipItemDATA.js
+++ assets/EquipItemDATA.js
@@ -3,91 +3,91 @@ export var items = {
         nombre: "Escudo de madera",
         descripcion: "Aumenta la vida y el daño en 35 y 15 puntos respectivamente",
         vidaEx: 25,
-        ataqEx: 15
+        ataqEx: 10
     },
     guanteCuero: {
         nombre: "Guante de cuero",
         descripcion: "Aumenta la vida y el daño en 15 y 35 puntos respectivamente",
         vidaEx: 15,
-        ataqEx: 25
+        ataqEx: 20
     },
     chalecoCuero: {
         nombre: "Chaleco de cuero",
         descripcion: "Aumenta la vida y el daño en 25 puntos",
         vidaEx: 20,
-        ataqEx: 20
+        ataqEx: 15
     },
     armaduraMadera: {
         nombre: "Armadura de madera",
         descripcion: "Aumenta la vida y el daño en 45 puntos",
-        vidaEx: 40,
-        ataqEx: 40
+        vidaEx: 30,
+        ataqEx: 30
     },
     escudoBronce: {
         nombre: "Escudo de bronce",
         descripcion: "Aumenta la vida y el daño en 55 y 25 puntos respectivamente",
-        vidaEx: 55,
+        vidaEx: 50,
         ataqEx: 25
     },
     guanteCueroPlus: {
         nombre: "Guante de cuero +",
         descripcion: "Aumenta la vida y el daño en 25 y 55 puntos respectivamente",
-        vidaEx: 35,
-        ataqEx: 50
+        vidaEx: 30,
+        ataqEx: 40
     },
     chalecoBronce: {
         nombre: "Chaleco de bronce",
         descripcion: "Aumenta la vida y el daño en 35 puntos",
         vidaEx: 45,
-        ataqEx: 45
+        ataqEx: 40
     },
     armaduraBronce: {
         nombre: "Armadura de bronce",
         descripcion: "Aumenta la vida y el daño en 50 puntos",
-        vidaEx: 60,
-        ataqEx: 50
+        vidaEx: 50,
+        ataqEx: 40
     },
     escudoHierro: {
         nombre: "Escudo de hierro",
         descripcion: "Aumenta la vida y el daño en 65 y 35 puntos respectivamente",
-        vidaEx: 70,
+        vidaEx: 60,
         ataqEx: 35
     },
     guanteHierro: {
         nombre: "Guante de hierro",
         descripcion: "Aumenta la vida y el daño en 35 y 65 puntos respectivamente",
         vidaEx: 40,
-        ataqEx: 60
+        ataqEx: 55
     },
     chalecoHierro: {
         nombre: "Chaleco de hierro",
         descripcion: "Aumenta la vida y el daño en 50 puntos",
-        vidaEx: 60,
-        ataqEx: 60
+        vidaEx: 55,
+        ataqEx: 50
     },
     armaduraHierro: {
         nombre: "Armadura de hierro",
         descripcion: "Aumenta la vida y el daño en 65 puntos",
-        vidaEx: 75,
-        ataqEx: 65
+        vidaEx: 70,
+        ataqEx: 60
     },
     escudoDiamante: {
         nombre: "Escudo de diamante",
         descripcion: "Aumenta la vida y el daño en 85 y 55 puntos respectivamente",
-        vidaEx: 90,
-        ataqEx: 55
+        vidaEx: 80,
+        ataqEx: 50
     },
     guanteDiamante: {
         nombre: "Guante de diamante",
         descripcion: "Aumenta la vida y el daño en 55 y 85 puntos respectivamente",
         vidaEx: 60,
-        ataqEx: 90
+        ataqEx: 70
     },
     chalecoDiamante: {
         nombre: "Chaleco de diamante",
         descripcion: "Aumenta la vida y el daño en 70 puntos",
-        vidaEx: 80,
-        ataqEx: 75
+        vidaEx: 70,
+        ataqEx: 65
     },
     armaduraDiamante: {
         nombre: "Armadura de diamante",
