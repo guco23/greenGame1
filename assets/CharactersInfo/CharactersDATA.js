4a44e9d277ecb24fa5270b176159300168e4445d
JoseAntonioCA <jocarm02@ucm.es>
2023-12-20 13:14:35 +0100

    Creados sprites de manos del jefe final, y cambiadas descripciones de personajes y habilidades

--- assets/CharactersInfo/CharactersDATA.js
+++ assets/CharactersInfo/CharactersDATA.js
@@ -2,97 +2,129 @@ export var personajes = {
     protagonista: {
         personalidad: "Verde", name: "Judini",
         maxHp: 100, atk: 33, def: 15, prefType: "",
-        imgLink: "Pers_Judini.png", idleImageLink: "Judini.png", crit: 10, MBTI: "Protagonista", descripcion: "Es rajoy (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Judini.png", idleImageLink: "Judini.png", crit: 10, MBTI: "Protagonista",
+        descripcion: "El main character",
+        descripcionHabilidad: "Lanza un potente espadazo a un enemigo, tras lo cual curará a sus aliados",
         imgIconLink: "IconJudini.png"
     },
     frikol: {
         personalidad: "Azul", name: "Frikol",
         maxHp: 100, atk: 33, def: 30, prefType: "",
-        imgLink: "Pers_Frikol.png", idleImageLink: "Frikol.png", crit: 10, MBTI: "Logista", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Frikol.png", idleImageLink: "Frikol.png", crit: 10, MBTI: "Logista",
+        descripcion: "Actually",
+        descripcionHabilidad: "Aumenta la defensa de todo su equipo proporcionalmente a su propia defensa",
         imgIconLink: "IconFrikol.png"
     },
     judioCesar: {
         personalidad: "Morado", name: "Judio César",
         maxHp: 125, atk: 40, def: 20, prefType: "",
-        imgLink: "Pers_Judio.png", idleImageLink: "JudioCaesar.png", crit: 10, MBTI: "Comandante", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Judio.png", idleImageLink: "JudioCaesar.png", crit: 10, MBTI: "Comandante",
+        descripcion: "Un gran emperador",
+        descripcionHabilidad: "Daña a todos los enemigos y les reduce levemente su ataque",
         imgIconLink: "IconJudioCaesar.png"
     },
     MrBean: {
         personalidad: "Amarillo", name: "Mr.Bean",
         maxHp: 80, atk: 55, def: 25, prefType: "",
-        imgLink: "Pers_MrBean.png", idleImageLink: "MrBean.png", crit: 10, MBTI: "Emprendedor", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_MrBean.png", idleImageLink: "MrBean.png", crit: 10, MBTI: "Emprendedor",
+        descripcion: "Influencer que puede desauciar hasta tu perro",
+        descripcionHabilidad: "Hace gran daño a un enemigo y le aumenta el ataque a sí mismo",
         imgIconLink: "IconMrBean.png"
     },
     bealonMusk: {
         personalidad: "Morado", name: "Bealon Musk",
         maxHp: 85, atk: 57, def: 40, prefType: "",
-        imgLink: "Pers_Bealon.png", idleImageLink: "BealonMusk.png", crit: 10, MBTI: "Innovador", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Bealon.png", idleImageLink: "BealonMusk.png", crit: 10, MBTI: "Innovador",
+        descripcion: "Bombardeen Twitter",
+        descripcionHabilidad: "Lanza un cohete que daña a todos los enemigos con posibilidad de aturdirlos",
         imgIconLink: "IconBealonMusk.png"
     },
     frijolConBotas: {
         personalidad: "Amarillo", name: "Frijol con Botas",
         maxHp: 70, atk: 75, def: 15, prefType: "",
-        imgLink: "Pers_FrijBotas.png", idleImageLink: "FrijolConBotas.png", crit: 10, MBTI: "Aventurero", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_FrijBotas.png", idleImageLink: "FrijolConBotas.png", crit: 10, MBTI: "Aventurero",
+        descripcion: "Ez, el Frijol con Botazzz, tururuuu",
+        descripcionHabilidad: "Golpea a un enemigo y le aplica mucho DoT",
         imgIconLink: "IconFrijolConBotas.png"
     },
     emmet: {
         personalidad: "Morado", name: "Emmet Beanckowski",
         maxHp: 120, atk: 35, def: 40, prefType: "",
-        imgLink: "Pers_Emmet.png", idleImageLink: "EmmetBeanckowski.png", crit: 10, MBTI: "Arquitecto", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Emmet.png", idleImageLink: "EmmetBeanckowski.png", crit: 10, MBTI: "Arquitecto",
+        descripcion: "¿Este pibe no era una pieza de construcción de juguete?",
+        descripcionHabilidad: "Ataca en area a varios enemigos, pero a uno en concreto le hace más daño",
         imgIconLink: "IconEmmetBeanckowski.png"
     },
     rajoy: {
         personalidad: "Azul", name: "Mariano Rajoy",
         maxHp: 220, atk: 25, def: 35, prefType: "",
-        imgLink: "Pers_Rajoy.png", idleImageLink: "MarianoRajoy.png", crit: 10, MBTI: "Ejecutivo", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Rajoy.png", idleImageLink: "MarianoRajoy.png", crit: 10, MBTI: "Ejecutivo",
+        descripcion: "Es rajoy, hace cosas",
+        descripcionHabilidad: "Aumenta el ataque de todo el equipo proporcional a cada valor de ataque",
         imgIconLink: "IconMarianoRajoy.png"
     },
     greta: {
         personalidad: "Verde", name: "Greta Judberg",
         maxHp: 90, atk: 25, def: 30, prefType: "",
-        imgLink: "Pers_Greta.png", idleImageLink: "GretaJudberg.png", crit: 10, MBTI: "Activista", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Greta.png", idleImageLink: "GretaJudberg.png", crit: 10, MBTI: "Activista",
+        descripcion: "Kucha contra la containación",
+        descripcionHabilidad: "Cura a todos los aliados y aumenta el ataque de uno en concreto",
         imgIconLink: "IconGretaJudberg.png"
     },
     selena: {
         personalidad: "Azul", name: "Selena Beamez",
         maxHp: 70, atk: 50, def: 50, prefType: "",
-        imgLink: "Pers_Selena.png", idleImageLink: "SelenaBeamez.png", crit: 10, MBTI: "Defensor", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Selena.png", idleImageLink: "SelenaBeamez.png", crit: 10, MBTI: "Defensor",
+        descripcion: "Hermosa mujer con hermosa voz",
+        descripcionHabilidad: "Aumenta bastante la defensa de un aliado y le da escudo",
         imgIconLink: "IconSelenaBeamez.png"
     },
     albert: {
         personalidad: "Morado", name: "Albert Beanstein",
         maxHp: 100, atk: 45, def: 30, prefType: "",
-        imgLink: "Pers_Albert.png", idleImageLink: "AlbertBeanstein.png", crit: 10, MBTI: "Lógico", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Albert.png", idleImageLink: "AlbertBeanstein.png", crit: 10, MBTI: "Lógico",
+        descripcion: "No es tu profe de ciencias, es EL profe de ciencias",
+        descripcionHabilidad: "Sus venenos hacen que su habilidad especial aplique DoT a todos los enemigo",
         imgIconLink: "IconAlbertBeanstein.png"
     },
     donald: {
         personalidad: "Azul", name: "Donald Bean",
         maxHp: 150, atk: 10, def: 40, prefType: "",
-        imgLink: "Pers_Donald.png", idleImageLink: "DonaldBean.png", crit: 10, MBTI: "Cónsul", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Donald.png", idleImageLink: "DonaldBean.png", crit: 10, MBTI: "Cónsul",
+        descripcion: "Antes era un pato",
+        descripcionHabilidad: "Su habilidad especial centrada en un aliado incrementa sus capacidades tanto ofensivas como defensivas",
         imgIconLink: "IconDonaldBean.png"
     },
     indiana: {
         personalidad: "Amarillo", name: "Indiana Beans",
         maxHp: 125, atk: 65, def: 20, prefType: "",
-        imgLink: "Pers_Indiana.png", idleImageLink: "IndianaBeans.png", crit: 10, MBTI: "Virtuoso", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Indiana.png", idleImageLink: "IndianaBeans.png", crit: 10, MBTI: "Virtuoso",
+        descripcion: "Intrépido y en busca de desafíos",
+        descripcionHabilidad: "Se aumenta mucho el ataque a sí mismo",
         imgIconLink: "IconIndianaBeans.png"
     },
     johnny: {
         personalidad: "Verde", name: "Johnny Bean",
         maxHp: 150, atk: 25, def: 25, prefType: "",
-        imgLink: "Pers_Johnny.png", idleImageLink: "JhonnyBean.png", crit: 10, MBTI: "Mediador", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Johnny.png", idleImageLink: "JhonnyBean.png", crit: 10, MBTI: "Mediador",
+        descripcion: "SU MUJER DE PEGABA (también es pirata y tiene una fábrica de chocolate)",
+        descripcionHabilidad: "Cura a un aliado en gran medida y le sana de cualquier efecto DoT negativo",
         imgIconLink: "IconJhonnyBean.png"
     },
     samba: {
         personalidad: "Amarillo", name: "Samba do Judía",
         maxHp: 135, atk: 60, def: 30, prefType: "",
-        imgLink: "Pers_Samba.png", idleImageLink: "SambaDoJudia.png", crit: 10, MBTI: "Animador", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_Samba.png", idleImageLink: "SambaDoJudia.png", crit: 10, MBTI: "Animador",
+        descripcion: "¡Sensacional, samba do judía!",
+        descripcionHabilidad: "Su potente ataque especial daña a un enemigo con posibilidades de aturdir",
         imgIconLink: "IconSambaDoJudia.png"
     },
     saulJudman: {
         personalidad: "Verde", name: "Saul Judman",
         maxHp: 90, atk: 45, def: 35, prefType: "",
-        imgLink: "Pers_SaulJudman.png", idleImageLink: "SaulJudman.png", crit: 10, MBTI: "Abogado", descripcion: "actually (poner aqui una descripcion del personaje", descripcionHabilidad: "este es un texto que describe la habilidad de frikol",
+        imgLink: "Pers_SaulJudman.png", idleImageLink: "SaulJudman.png", crit: 10, MBTI: "Abogado",
+        descripcion: "Gran abogado",
+        descripcionHabilidad: "Cura a un aliado y le proporciona un escudo extra",
         imgIconLink: "IconSaulJudman.png"
     }
 }
